44bcf762bc813b45630a685ca1ae1db242726cf8
abcd <grayriverza@gmail.com>
2025-12-15 21:19:20 +0500

    structure fondation complete

--- lava.cpp
+++ lava.cpp
@@ -1,5 +1,36 @@
 #include<iostream>
 using namespace std;
+
+struct age{
+    int day , month , year;
+}
+;
+
+struct add{
+    string padr , radr;
+}
+;
+
+struct cont{
+    string phone1 , phone2 , email;
+}
+;
+struct worker{
+    string NAME ;
+    age AGE;
+    add ADDRESS;
+    cont REFRENCES;
+    string JOB_status;
+
+
+};
+
+
+
+
+
+
+
 int main(){
-    cout << "sdccccccdasdmmmmmmmmmmmmmmmmmmmmmsdasdwowow";
+
 }
